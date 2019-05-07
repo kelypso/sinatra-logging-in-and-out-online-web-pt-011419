866849a8aa6b3beaf8320d4205e2562c09a83081
Kelsey Low <kklowlow@gmail.com>
2019-05-07 16:59:53 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,7 +1,6 @@
 class Helpers < ActiveRecord::Base
     def self.current_user(session)
        user = User.find(session[:user_id])
-       #binding.pry
     end
 
     def self.is_logged_in?(session)
